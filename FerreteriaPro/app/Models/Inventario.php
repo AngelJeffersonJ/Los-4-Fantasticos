11734b551544e21392af2fa0d29ef626cfb9a753
Nataly <diazdeleondolores12@gmail.com>
2024-05-12 13:08:54 -0600

    Models

---
+++ FerreteriaPro/app/Models/Inventario.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventario extends Model
+{
+    protected $table = 'inventario';
+    protected $primaryKey = 'ID_Inventario';
+    
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, 'ID_Producto', 'id');
+    }
+}
+
