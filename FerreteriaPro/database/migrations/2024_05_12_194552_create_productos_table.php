59e624eb1cd6a81c872ba5284eb7b1f8fbe1fd71
Nataly <diazdeleondolores12@gmail.com>
2024-05-12 13:47:09 -0600

    m

--- FerreteriaPro/database/migrations/2024_05_12_194552_create_productos_table.php
+++ FerreteriaPro/database/migrations/2024_05_12_194552_create_productos_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('proveedores', function (Blueprint $table) {
+        Schema::create('productos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('contacto')->nullable();
-            $table->string('telefono')->nullable();
-            $table->string('correo_electronico')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('proveedores');
+        Schema::dropIfExists('productos');
     }
 };
