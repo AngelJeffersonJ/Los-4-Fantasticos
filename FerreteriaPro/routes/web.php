eaf33b72e6a2fe37db779c6eda1ac7d0cf23000c
AngelJeffersonJ <jeffersonreincarnation@hotmail.com>
2024-05-13 00:07:36 -0600

    rutas 2.0

--- FerreteriaPro/routes/web.php
+++ FerreteriaPro/routes/web.php
@@ -1,24 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\VentaController;
+use App\Http\Controllers\VentaDetalleController;
+use App\Http\Controllers\InventarioController;
 
-// Rutas para el controlador ProductoController
-Route::resource('productos', 'ProductoController');
+// Rutas para el controlador de Producto
+Route::resource('productos', ProductoController::class);
 
-// Rutas para el controlador CategoriaController
-Route::resource('categorias', 'CategoriaController');
+// Rutas para el controlador de Categoría
+Route::resource('categorias', CategoriaController::class);
 
-// Rutas para el controlador ProveedorController
-Route::resource('proveedores', 'ProveedorController');
+// Rutas para el controlador de Proveedor
+Route::resource('proveedores', ProveedorController::class);
 
-// Rutas para el controlador ClienteController
-Route::resource('clientes', 'ClienteController');
+// Rutas para el controlador de Cliente
+Route::resource('clientes', ClienteController::class);
 
-// Rutas para el controlador VentaController
-Route::resource('ventas', 'VentaController');
+// Rutas para el controlador de Venta
+Route::resource('ventas', VentaController::class);
 
-// Rutas para el controlador VentaDetalleController
-Route::resource('venta_detalles', 'VentaDetalleController');
+// Rutas para el controlador de Detalle de Venta
+Route::resource('venta_detalles', VentaDetalleController::class);
 
-// Rutas para el controlador InventarioController
-Route::resource('inventario', 'InventarioController');
+// Rutas para el controlador de Inventario
+Route::resource('inventario', InventarioController::class);
