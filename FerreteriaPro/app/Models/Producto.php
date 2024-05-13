696fe0b6dab5635b96484a63a866681b2c9fe562
Nataly <diazdeleondolores12@gmail.com>
2024-05-13 07:56:08 -0600

    modelo producto

--- FerreteriaPro/app/Models/Producto.php
+++ FerreteriaPro/app/Models/Producto.php
@@ -1,27 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
     protected $fillable = [
-        'nombre', 'descripcion', 'precio_unitario', 'stock', 'id_categoria', 'id_proveedor'
+        'nombre', 'descripcion', 'precio_unitario', 'stock', 'id_categoria', 'id_proveedor',
     ];
 
     public function categoria()
     {
-        return $this->belongsTo('App\Categoria', 'id_categoria');
+        return $this->belongsTo(Categoria::class, 'id_categoria');
     }
 
     public function proveedor()
     {
-        return $this->belongsTo('App\Proveedor', 'id_proveedor');
-    }
-
-    public function ventas()
-    {
-        return $this->hasMany('App\VentaDetalle', 'id_producto');
+        return $this->belongsTo(Proveedor::class, 'id_proveedor');
     }
 }
