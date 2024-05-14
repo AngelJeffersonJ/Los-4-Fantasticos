924e7ca7bd9d1fa2dc7c61d88e8b04af15c3bea3
AngelJeffersonJ <jeffersonreincarnation@hotmail.com>
2024-05-13 20:19:06 -0600

    Modelo productos

--- FerreteriaPro/app/Models/Producto.php
+++ FerreteriaPro/app/Models/Producto.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
     protected $fillable = [
-        'nombre', 'descripcion', 'precio_unitario', 'stock'
+        'nombre',
+        'descripcion',
+        'precio_unitario',
+        'stock',
     ];
 }
