33dbbd4e243af2dbba2c240734eb858287fec379
AngelJeffersonJ <jeffersonreincarnation@hotmail.com>
2024-05-15 10:30:12 -0600

    productoController

--- FerreteriaPro/app/Http/Controllers/ProductoController.php
+++ FerreteriaPro/app/Http/Controllers/ProductoController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Producto;
-use App\Models\Categoria;
-use App\Models\Proveedor;
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
     public function index()
     {
         $productos = Producto::all();
-        $categorias = Categoria::all();
-        $proveedores = Proveedor::all();
-        return view('productos', compact('productos', 'categorias', 'proveedores'));
+        return view('productos.index', compact('productos'));
+    }
+
+    public function create()
+    {
+        return view('productos.create');
     }
 
     public function store(Request $request)
@@ -30,6 +31,16 @@ class ProductoController extends Controller
 
         Producto::create($request->all());
         return redirect()->route('productos.index')->with('success', 'Producto creado exitosamente.');
+    }
+
+    public function show(Producto $producto)
+    {
+        return view('productos.show', compact('producto'));
+    }
+
+    public function edit(Producto $producto)
+    {
+        return view('productos.edit', compact('producto'));
     }
 
     public function update(Request $request, Producto $producto)
