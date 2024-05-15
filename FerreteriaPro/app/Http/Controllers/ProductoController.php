187056a4ec6b53448593af50b5bdac79e2eaa9da
AngelJeffersonJ <jeffersonreincarnation@hotmail.com>
2024-05-15 11:44:02 -0600

    comtroller

--- FerreteriaPro/app/Http/Controllers/ProductoController.php
+++ FerreteriaPro/app/Http/Controllers/ProductoController.php
@@ -7,12 +7,6 @@ use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
-    public function index()
-    {
-        $productos = Producto::all();
-        return view('productos.index', compact('productos'));
-    }
-
     public function create()
     {
         return view('productos.create');
