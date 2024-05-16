936aadf4d4c202fabaf65c5c79e0bff35f74bdd8
AngelJeffersonJ <jeffersonreincarnation@hotmail.com>
2024-05-15 22:53:09 -0600

    proveedor modelo

--- FerreteriaPro/app/Models/Proveedor.php
+++ FerreteriaPro/app/Models/Proveedor.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Proveedor extends Model
 {
-    use HasFactory;
+    protected $table = 'proveedores';
 
-    protected $fillable = ['nombre', 'direccion', 'telefono'];
+    protected $fillable = [
+        'nombre',
+        'direccion',
+        'telefono'
+    ];
 }
