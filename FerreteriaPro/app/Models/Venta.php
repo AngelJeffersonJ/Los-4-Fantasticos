3dfad7ff94b314d9977a59cdc5e2bbd5c93ec42b
Nataly <diazdeleondolores12@gmail.com>
2024-05-16 00:24:15 -0600

    modelo (venta)

--- FerreteriaPro/app/Models/Venta.php
+++ FerreteriaPro/app/Models/Venta.php
@@ -6,36 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
-    /**
-     * The tablxe associated with the model.
-     *
-     * @var string
-     */
     protected $table = 'ventas';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'fecha',
         'id_cliente',
     ];
 
-    /**
-     * Get the cliente that owns the venta.
-     */
+    protected $dates = [
+        'fecha',
+    ];
+
     public function cliente()
     {
         return $this->belongsTo(Cliente::class, 'id_cliente');
-    }
-
-    /**
-     * Get the detalles for the venta.
-     */
-    public function detalles()
-    {
-        return $this->hasMany(VentaDetalle::class, 'id_venta');
     }
 }
